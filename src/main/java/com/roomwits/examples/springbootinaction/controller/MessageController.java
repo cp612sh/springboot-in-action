d197a6433bd85f0170aeac073dc43ce409d67982
CaPaM <cp612sh@users.noreply.github.com>
2022-06-23 17:29:30 +0800

    feat:add object method to return build-in object

--- src/main/java/com/roomwits/examples/springbootinaction/controller/MessageController.java
+++ src/main/java/com/roomwits/examples/springbootinaction/controller/MessageController.java
@@ -1,17 +1,33 @@
 package com.roomwits.examples.springbootinaction.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
-    @GetMapping("/")							// 映射的访问路径
-	public String home() {
-		return "www.roomwits.com";
-	}
+    @GetMapping("/") 
+    public String home() {
+        return "www.roomwits.com";
+    }
 
-    @GetMapping("/echo")						// 映射的访问路径
-    public String echo(String msg) {
+    @GetMapping("/echo/{message}") 
+    public String echo(@PathVariable("message") String msg) {
         return "[Echo]:" + msg;
+    }
+
+    @GetMapping("/object")
+    public Object object(HttpServletRequest request, HttpServletResponse response){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Client IP:", request.getRemoteAddr());
+        map.put("Client encoding", request.getCharacterEncoding());
+        map.put("SessionID", request.getSession().getId());
+        map.put("Project real path", request.getServletContext().getRealPath("/"));
+        return map;
     }
 }
