8d1390d6e2e297fb92765be02d27eacdf4ae797d
CaPaM <cp612sh@users.noreply.github.com>
2022-06-23 14:29:09 +0800

    feat: add testcase for home method

--- src/test/java/com/roomwits/examples/springbootinaction/SpringbootInActionApplicationTests.java
+++ src/test/java/com/roomwits/examples/springbootinaction/SpringbootInActionApplicationTests.java
@@ -1,13 +1,26 @@
 package com.roomwits.examples.springbootinaction;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
+import junit.framework.TestCase;
+
+
+@SpringBootTest(classes = SpringbootInActionApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 class SpringbootInActionApplicationTests {
 
+	@Autowired
+	SpringbootInActionApplication applicationController;
+
 	@Test
-	void contextLoads() {
+	void testHome() {
+		TestCase.assertEquals(this.applicationController.home(), "www.roomwits.com");
 	}
 
 }
