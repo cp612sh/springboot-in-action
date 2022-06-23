bf3f49247f511137fec610a72595e1cf59c1add4
CaPaM <CaPaM@mba>
2022-06-23 22:12:25 +0800

    chore: uses ServletRequestAttributes instead of

--- src/main/java/com/roomwits/examples/springbootinaction/controller/MessageController.java
+++ src/main/java/com/roomwits/examples/springbootinaction/controller/MessageController.java
@@ -8,24 +8,30 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 public class MessageController {
-    @GetMapping("/") 
+    @GetMapping("/")
     public String home() {
         return "www.roomwits.com";
     }
 
-    @GetMapping("/echo/{message}") 
+    @GetMapping("/echo/{message}")
     public String echo(@PathVariable("message") String msg) {
         return "[Echo]:" + msg;
     }
 
     @GetMapping("/object")
-    public Object object(HttpServletRequest request, HttpServletResponse response){
+    public Object object() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getRequest();
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                .getResponse();
         HashMap<String, String> map = new HashMap<>();
         map.put("Client IP:", request.getRemoteAddr());
-        map.put("Client encoding", request.getCharacterEncoding());
+        map.put("Client encoding", response.getCharacterEncoding());
         map.put("SessionID", request.getSession().getId());
         map.put("Project real path", request.getServletContext().getRealPath("/"));
         return map;
